0a13fa1a5a3e8d6072b77fb88395d0b5dc46c7f7
Gabriela Pires <gabrielap.informatica@gmail.com>
2022-03-31 12:31:57 -0300

    finalizando textbox

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -15,5 +15,30 @@ describe('Work with basic elements', () => {
         cy.get('[href="#"]').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
     })
+
+    it.only('Text field', () => {
+        cy.get('#formNome').type('Cypress test')
+        cy.get('#formNome').should('have.value', 'Cypress test')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .type('textarea')
+            .should('have.value', 'textarea')
+        //
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('hello')
+        //
+
+        cy.get('[data-cy="dataSobrenome"]')
+            .type('Teste12345{backspace}{backspace}')
+            //da pra ver todos os comandos, é só escrever o {backspace} errado e executar
+            .should('have.value', 'Teste123')
+        //
+
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', {delay:100})
+            .should('have.value', 'acerto')
+    })
 })
 
